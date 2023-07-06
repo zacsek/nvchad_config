376c32844f049ddeffd40644c44f1f176cf2f88e
Zoltan Szocs <zacsek@gmail.com>
2023-07-06 22:35:45 +0200

    WSL clipboard, conditioned to load under wsl only

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
-vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/custom/wsl_clipboard.vim")
+
+if vim.fn.has("wsl") == 1 then
+  vim.cmd("source " .. vim.fn.stdpath("config") .. "/lua/custom/wsl_clipboard.vim")
+end
